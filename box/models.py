6e73070c082c347dd6feac3d1e90f63384f518ad
behdad hajipour <bhajipour@avodaq.com>
2023-02-28 14:49:17 +0100

    updated views.py
    added queryset and template_name

--- box/models.py
+++ box/models.py
@@ -7,7 +7,7 @@ class LeitnerBox(models.Model):
     word = models.CharField(max_length=50, blank=False, null=False)
     translate = models.CharField(max_length=50, blank=False, null=False)
     pronounce = models.CharField(max_length=50, blank=True, null=True)
-    counter = models.IntegerField( blank=False, null=False)
+    counter = models.IntegerField(blank=False, null=False)
     example = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
--- box/views.py
+++ box/views.py
@@ -1,5 +1,5 @@
 from django_tables2 import SingleTableView
-from . import tables
+from . import tables, models
 
 
 class LeitnerBoxList(SingleTableView):
@@ -7,3 +7,5 @@ class LeitnerBoxList(SingleTableView):
 
     table_class = tables.LeitnerBoxTable
     table_pagination = {"per_page": 30}
+    queryset = models.LeitnerBox.objects.all()
+    template_name = "leitner_box/leitner_box.html"
