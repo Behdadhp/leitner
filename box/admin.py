159c4e198749284e15d93be08e5b632637ce2107
behdad hajipour <bhajipour@avodaq.com>
2023-02-28 12:53:53 +0100

    added leitner box admin page

--- box/admin.py
+++ box/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class LeitnerBoxAdmin(admin.ModelAdmin):
+    """class representing of Leitner Box admin page"""
+
+    list_display = ("word", "translate", "pronounce", "counter", "example")
+    list_display_links = ("word", "translate", "pronounce", "counter", "example")
+    ordering = ("_counter",)
+
+
+admin.site.register(models.LeitnerBox, LeitnerBoxAdmin)
